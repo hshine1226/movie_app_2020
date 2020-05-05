3bd1aec11e7838e37dd2b2f9c12aa48af5515a0b
hshine1226 <hshine1226@gmail.com>
2020-05-05 19:32:56 +0900

    JSX와 Props를 사용해서 Component를 재사용 하는 방법 학습

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+function Food({ favorite }) {
+  console.log(favorite);
+  return <h2>I like {favorite}</h2>;
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <Food favorite="Chicken" />
+      <Food favorite="된장찌게" />
+      <Food favorite="라면" />
+      <Food favorite="삼겹살" />
     </div>
   );
 }
 
 export default App;
+
+// JSX는 단지 HTML + JavaScript의 조합이다
+// Component는 대문자로 시작해야 한다.
+// Component는 props를 통해 정보를 보낼 수 있다.
