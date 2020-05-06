ee93e3a2a760f86b1b0546e61106e7c28e16efa0
hshine1226 <hshine1226@gmail.com>
2020-05-07 02:25:23 +0900

    형식: CSS 작업

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./reset.css";
 import "./Movie.css";
 
 // State가 필요하지 않으면 class component를 사용할 필요가 없다.
@@ -8,8 +9,9 @@ function Movie({ year, title, summary, poster, genres }) {
     <div className="movie">
       <img src={poster} alt={title} title={title}></img>
       <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
+        <h3 className="movie__title">{title}</h3>
+        <p className="movie__summary">{summary.substring(0, 300)}...</p>
         <ul className="movie__genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres__genre">
@@ -17,7 +19,6 @@ function Movie({ year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
