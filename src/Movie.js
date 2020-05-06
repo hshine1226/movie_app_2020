3d53c0c3c1610ccd92e3ca2d77fa86ca6ac1ddee
hshine1226 <hshine1226@gmail.com>
2020-05-07 01:15:07 +0900

    기능: 영화 장르 추가

--- src/Movie.js
+++ src/Movie.js
@@ -3,14 +3,21 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 // State가 필요하지 않으면 class component를 사용할 필요가 없다.
-function Movie({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title}></img>
-      <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie__summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   );
@@ -22,6 +29,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
