e001f738d23d3c831464d2723b3885f2980fa709
hshine1226 <hshine1226@gmail.com>
2020-05-06 16:26:53 +0900

    #3.3 Planning the Movie Component - javaScript if문을 이용해서 Loading중과 Loading완료를 표현하고, setTimeout을 이용해서 딜레이를 준후에 표현했다.

--- src/App.js
+++ src/App.js
@@ -2,33 +2,18 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
+    isLoading: true,
+    movies: [],
   };
 
   componentDidMount() {
-    console.log("Component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 2000);
   }
-
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
 }
 
