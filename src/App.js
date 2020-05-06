86f6266b5e152d3f0f9b4583ad5460ae8d235fce
hshine1226 <hshine1226@gmail.com>
2020-05-06 13:29:50 +0900

    #3.0 Class Component and State - State는 동적 데이터와 작업할 때 사용한다.
    - function component를 class component로 바꾼다.
    
    - React Component는 return을 가지지 않는다. render를 가진다. 왜냐하면 function이 아니기 때문이다
    
    - React는 자동적으로 class component의 render method를 실행한다.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "김치",
-    image:
-      "https://images.unsplash.com/photo-1583224944844-5b268c057b72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    rating: 4.8,
-  },
-  {
-    id: 2,
-    name: "삼겹살",
-    image:
-      "https://images.unsplash.com/photo-1550388342-b3fd986e4e67?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "된장찌개",
-    image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/08/24c312f82313faaf1e4d5ef98761efcb1.jpg",
-    rating: 4.5,
-  },
-  {
-    id: 4,
-    name: "라면",
-    image:
-      "https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
-    rating: 3.5,
-  },
-];
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-function Food({ name, picture, rating }) {
-  console.log(name);
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>Raing {rating}/5</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => {
-        return (
-          <Food
-            key={dish.id}
-            name={dish.name}
-            picture={dish.image}
-            rating={dish.rating}
-          />
-        );
-      })}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
-
-// JSX는 단지 HTML + JavaScript의 조합이다
-// Component는 대문자로 시작해야 한다.
-// Component는 props를 통해 정보를 보낼 수 있다.
