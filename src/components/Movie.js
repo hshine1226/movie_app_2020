04a0a2b18caa6b243cd99c9366b346c9edc747b0
hshine1226 <hshine1226@gmail.com>
2020-05-07 19:04:16 +0900

    #6.4 Redirecting - movie detail 화면은 클릭을 통해서 들어가면 state를 받아올 수 없기 때문에 state가 undefined이면 자동적으로 홈으로 Redirect 시켜준다.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,26 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import "./reset.css";
 import "./Movie.css";
 
-// State가 필요하지 않으면 class component를 사용할 필요가 없다.
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title}></img>
-      <div className="movie__data">
-        <h5 className="movie__year">{year}</h5>
-        <h3 className="movie__title">{title}</h3>
-        <p className="movie__summary">{summary.substring(0, 300)}...</p>
-        {/* <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul> */}
+    <Link
+      to={{
+        pathname: `/movie/detail/${id}`,
+        state: {
+          year,
+          title,
+          summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
